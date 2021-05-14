3cac6c8fb3724181cd6adda876b361f2b8e1b821
Juan Loaiza <joumanmaster_@outlook.es>
2021-05-14 10:04:42 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -3,19 +3,17 @@ const app = express();
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load('./docs/swagger.yml');
-const cors =  require('cors');
+const cors =  require('./middlewares/cors')
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 
-app.use(cors());
-
 const emailRoutes = require("./routes/email");
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-app.use('/email', emailRoutes);
+app.use('/api-docs', cors,swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+app.use('/email', cors ,emailRoutes);
 
 
 module.exports = app;
